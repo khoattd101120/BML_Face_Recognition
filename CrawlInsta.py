42281ece1a9e51f479e00dd6b52797071f70e4bf
theduyit <nduy250299@gmail.com>
2020-12-19 16:46:26 +0700

    fix 'id' error, update code to remove image not contain face

--- CrawlInsta.py
+++ CrawlInsta.py
@@ -1,10 +1,13 @@
 import os
 import glob
+import dlib
+import cv2
 
 class CrawlInsta:
     def __init__(self):
         self.root = os.getcwd()
         self.path = os.path.join(self.root, 'images')
+        self.face_detector = dlib.get_frontal_face_detector()
         if not os.path.exists(self.path):
             os.makedirs(self.path)
         # change the current working directory
@@ -24,7 +27,18 @@ class CrawlInsta:
         for redundant in glob.glob(os.path.join(self.path, folder, '*.*')):
             if not (redundant.endswith('.jpg') or redundant.endswith('.png')):
                 os.remove(redundant)
-        os.remove(os.path.join(self.path, folder, 'id'))
+            else: # remove image not contains face
+                img = cv2.imread(redundant)
+                small_frame = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
+                rgb_small_frame = small_frame[:, :, ::-1]
+                faces = self.face_detector(rgb_small_frame, 1)
+                if len(faces) == 0:
+                    os.remove(redundant)
+        try:
+            os.remove(os.path.join(self.path, folder, 'id'))
+        except:
+            pass
+
 if __name__ == '__main__':
     crawler = CrawlInsta()
-    crawler.load_image('taylorswift', update = False)
+    crawler.load_image('taylorswift', update = True)
